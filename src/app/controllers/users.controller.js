088f75e2a448f4c40a43661f1a4e7a92d1d20efe
jodsab <85377591+jodsab@users.noreply.github.com>
2022-11-17 11:59:58 -0500

    dsa

--- src/app/controllers/users.controller.js
+++ src/app/controllers/users.controller.js
@@ -23,7 +23,7 @@ export const getLoginUser = async (req, res) => {
         );
         if (validateUser) {
           /* const token = jwt.sign({id}) */
-          //ya si no ves esto no se
+          //ya si no ves esto no se sadasdsa
           jwt.sign(rows[0], process.env.JWT_SK, (err, token) => {
             if (err) {
               res.status(500).send({ msg: "Error al generar token" });
